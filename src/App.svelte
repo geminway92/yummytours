49aee88ea87bf966df5b1ff365bc3f61c32ff8be
Gema Martin <geminway@live.com>
2022-06-09 17:17:46 +0200

    Added meta in index.html

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,9 @@
   import Home from "./views/Home.svelte";
   import About from "./views/About.svelte";
   import RoutesTour from "./views/RoutesTour.svelte";
-  import Meta from './lib/Meta.svelte';
   import RouteId from "./views/RouteId.svelte";
 
   let isActiveMenu: boolean = false;
-
-  const metadata = {
-		title: 'Yummy Tours',
-		description: 'Somos una empresa de rutas gastronomicas por españa que fusiona gastronomia y cultura al mejor precio y con experiencias unicas en el sector , nuestra idea de negocio  surge de la necesidadde mezclar estos dos mundos que nos apasionan y crear una agencia dedicada exclusivamente para ello  , descubre que te ofrecemos en nuestra oficina de benalmadena o si lo prefieres en esta web , en la app o en nuestras redes , atrevete a saborear españa con nsostros y no te errepentiras',
-		image: 'https://res.cloudinary.com/ddn278n2q/image/upload/v1654635403/meta-img/v6lkixqrchkl0q9glnea.jpg',
-		imageAlt: 'Yummy Tours Logo',
-		url:'https://yummytours.netlify.app'
-	}
 
   const showMenu = () => {
     isActiveMenu = !isActiveMenu;
@@ -25,7 +16,6 @@
 </script>
 
 <div>
-  <Meta {metadata}/>
 <Router>
   <nav class="navbar navbar-expand-lg bg-light fixed-top shadow-sm">
     <div class="container-fluid">
